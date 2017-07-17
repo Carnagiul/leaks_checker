4bb35420c3af1562f504075db0c54ec24558ee96
piquerue <pierrequeruel@gmail.com>
2017-07-17 09:13:38 +0200

    test tmp

---
+++ setup/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Bienvenue sur l'installation de project_checker.\n"
+echo "ce project va creer des alias temporaire pour la correction.\n"
+alias norme_color="/tmp/leaks_checker/norminette_colored/norminette_colored"

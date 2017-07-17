1ae5b3a0e30b1752583cf70904152e756984c600
piquerue <pierrequeruel@gmail.com>
2017-07-17 08:56:07 +0200

    protect with param

--- leaks.sh
+++ leaks.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-PROCESSID=$(pgrep -f $1)
-#PROCESSID=$(ps | grep "wolf3d" | grep -v "grep" | sed 's/^[ ]*//g' | cut -d" " -f1)
-leaks -q $PROCESSID
+if [ $# -ge 1 ]
+then
+	PROCESSID=$(pgrep -f $1)
+	#PROCESSID=$(ps | grep "wolf3d" | grep -v "grep" | sed 's/^[ ]*//g' | cut -d" " -f1)
+	leaks -q $PROCESSID
+else
+	echo "Usage: ./leaks.sh <projectname>"
+fi
